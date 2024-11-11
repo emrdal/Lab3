ed4ebbf0b226d98d666719e63dd804fe20ffb304
emrdal <emrendeye06@gmail.com>
2024-11-11 22:39:26 +0100

    test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -48,6 +48,11 @@ public class CalculatorTest {
         int actual = calculator.multiply(8,2);
         Assert.assertEquals(16,actual);
     }
+    @Test
+    public void testMultiply2_(){
+        int actual = calculator.multiply(8,2);
+        Assert.assertEquals(17,actual);
+    }
 
 
 }
